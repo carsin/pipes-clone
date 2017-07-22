bbea276f6209614887490094f5db935905bc203b
carsin <carsin@users.noreply.github.com>
2017-07-22 11:02:22 -1000

    Complete

--- code.js
+++ code.js
@@ -11,7 +11,13 @@ var canvas = {
 
         canvas.ele.width = canvas.width;
         canvas.ele.height = canvas.height;
+
+        resetLines();
     },
+}
+
+function genRandColor() {
+    return "rgb(" + Math.round(Math.random() * 255) + ", " + Math.round(Math.random() * 255) + ", " + Math.round(Math.random() * 255) +")";
 }
 
 window.onload = function() {
@@ -20,13 +26,22 @@ window.onload = function() {
     canvas.gfx = canvas.ele.getContext("2d");
 
     canvas.setDimensions();
-    
-    canvas.gfx.fillStyle = "black";
-    canvas.gfx.fillRect(0, 0, canvas.width, canvas.height);
 
-    var testLine = new Line("blue", 5, 7, canvas.width / 2, canvas.height / 2, 2);
+    for (var i = 0; i < 10; i++) {
+        new Line(genRandColor(), 6, 9, canvas.width / 2, canvas.height / 2, 4);
+    }
+
+
     setInterval(function() {
-        testLine.update();
-        testLine.render();
+        let numLines = lines.length;
+        for (var i = 0; i < numLines; i++) {
+            lines[i].update();
+            lines[i].render();
+        }
     }, 1000/60);
+
+    setInterval(resetLines, 10000);
+
+    window.addEventListener("resize", canvas.setDimensions);
 }
+
--- line.js
+++ line.js
@@ -1,5 +1,15 @@
 var lines = [];
 
+function resetLines() {
+    canvas.gfx.fillStyle = "black"; 
+    canvas.gfx.fillRect(0, 0, canvas.width, canvas.height);
+
+    let numLines = lines.length;
+    for (var i = 0; i < numLines; i++) {
+        lines[i].reset();
+    }
+}
+
 function Line(color, height, width, x, y, speed) {
     this.color = color;
     this.height = height;
@@ -8,7 +18,6 @@ function Line(color, height, width, x, y, speed) {
     this.y = y;
     this.speed = speed;
     this.dir = Math.round(Math.random() * 3);
-    this.dirChangeCount = 0;
 
     this.render = function() {
         // canvas.gfx.fillStyle = "black";
@@ -18,11 +27,26 @@ function Line(color, height, width, x, y, speed) {
     }
 
     this.update = function() {
-        this.dirChangeCount++;
-        if (this.dirChangeCount >= 20) {
+        let chanceToChangeDir = Math.random();
+        if (chanceToChangeDir >= 0.95) {
             this.dirChangeCount = 0;
-            this.dir = Math.round(Math.random() * 3);
-            console.log(this.dir);
+            let newVar = Math.round(Math.random() * 3);
+            switch (this.dir) {
+                case 0:
+                    if (newVar !== 2) this.dir = newVar;
+                    break;
+                case 1:
+                    if (newVar !== 3) this.dir = newVar;
+                    break;
+                case 2:
+                    if (newVar !== 0) this.dir = newVar;
+                    break;
+                case 3:
+                    if (newVar !== 1) this.dir = newVar;
+                    break;
+                default:
+                    console.log("Error: Bad input");
+            }
         }
 
         //     ^
@@ -40,4 +64,13 @@ function Line(color, height, width, x, y, speed) {
         }
 
     }
+
+    this.reset = function() {
+        console.log("reset");
+        this.x = canvas.width / 2 - this.width / 2;
+        this.y = canvas.height / 2 - this.height / 2
+    }
+
+    lines.push(this); 
+
 }
